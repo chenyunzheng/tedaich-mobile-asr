66e62782ef99fb363692ea2ff326e5e7d46aefbe
chris chen <y_zhengchen@163.com>
2019-11-13 23:42:23 -0500

    audio record function 65%

--- app/src/main/java/com/tedaich/mobile/asr/service/WriteAudioService.java
+++ app/src/main/java/com/tedaich/mobile/asr/service/WriteAudioService.java
@@ -1,13 +1,18 @@
 package com.tedaich.mobile.asr.service;
 
+import android.util.Log;
+
 import com.tedaich.mobile.asr.util.AndroidUtils;
 import com.tedaich.mobile.asr.util.AudioUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Date;
 import java.util.Vector;
 
 class WriteAudioService implements Runnable {
+
+    private static final String LOG_TAG = "WriteAudioService";
 
     private RecordAudioTask recordAudioTask;
 
@@ -17,15 +22,31 @@ class WriteAudioService implements Runnable {
 
     @Override
     public void run() {
-        String audioDir = AudioUtils.getAudioDirectory() + File.separatorChar + "语音" + new Date().toString();
-
-        while (!recordAudioTask.getIsDelete().get() && !recordAudioTask.getIsSave().get()){
-            //write audio data into file
-            if (recordAudioTask.getIsRecording().get()){
-                Vector<byte[]> audioData = recordAudioTask.getAudioData();
-
-            }
+        String defaultAudioName = "语音_" + AndroidUtils.formatDate(new Date());
+        String audioPath = AudioUtils.getAudioDirectory() + File.separatorChar + defaultAudioName;
+        String pcmAudioPath = audioPath + ".pcm";
+        String wavAudioPath = audioPath + ".wav";
+        File pcmAudioFile = new File(pcmAudioPath);
+        if (pcmAudioFile.exists()){
+            pcmAudioFile.delete();
         }
+        try (FileOutputStream fos = new FileOutputStream(pcmAudioFile)){
+            while (!recordAudioTask.getIsDelete().get() && !recordAudioTask.getIsSave().get()){
+                //write audio data into file
+                if (recordAudioTask.getIsRecording().get()){
+                    Vector<byte[]> audioData = recordAudioTask.getAudioData();
+                    for (byte[] data : audioData) {
+                        fos.write(data);
+                    }
+                    fos.flush();
+                }
+            }
+            AudioUtils.convertPCMToWAV(pcmAudioPath, wavAudioPath);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "error in writing audio data to file", e);
+        }
+
+
     }
 
     public static void main(String[] args) {
--- app/src/main/java/com/tedaich/mobile/asr/util/AudioUtils.java
+++ app/src/main/java/com/tedaich/mobile/asr/util/AudioUtils.java
@@ -1,6 +1,8 @@
 package com.tedaich.mobile.asr.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class AudioUtils {
 
@@ -31,7 +33,28 @@ public class AudioUtils {
         }
     }
 
+    /**
+     * get audio directory
+     * @return
+     */
     public static String getAudioDirectory(){
         return AudioUtils.audioDirectory;
     }
+
+    public static void convertPCMToWAV(String pcmPath, String wavPath) throws Exception {
+        FileInputStream fis = new FileInputStream(pcmPath);
+        FileOutputStream fos = new FileOutputStream(wavPath);
+        byte[] buf = new byte[1024 * 1000];
+        int size = fis.read(buf);
+        int PCMSize = 0;
+        while (size != -1) {
+            PCMSize += size;
+            size = fis.read(buf);
+        }
+        fis.close();
+
+
+
+    }
+
 }
